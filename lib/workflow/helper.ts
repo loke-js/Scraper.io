49ba93c34bc064cdfc5364099f8273e6dc15f081
loke-js <lokendratiwari22844@gmail.com>
2025-11-19 11:20:13 +0530

    Cron Scheduler Working in Prod

--- lib/workflow/helper.ts
+++ lib/workflow/helper.ts
@@ -2,6 +2,7 @@ import { AppNode } from "@/types/appNode";
 import { TaskRegistry } from "./task/registry";
 
 export function CalculateWorkflowCost(nodes:AppNode[]){
-    console.log(TaskRegistry[nodes[1].data.type].credits);
+    console.log(nodes);
+    console.log(TaskRegistry[nodes[0].data.type].credits);
     return nodes.reduce((acc,node)=>acc+(TaskRegistry[node.data.type].credits),0);
 }
