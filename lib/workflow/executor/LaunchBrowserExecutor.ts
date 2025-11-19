e961ee893c79b1b603604d4de33cf6e194479013
loke-js <lokendratiwari22844@gmail.com>
2025-11-19 12:25:11 +0530

    Trying Browserless

--- lib/workflow/executor/LaunchBrowserExecutor.ts
+++ lib/workflow/executor/LaunchBrowserExecutor.ts
@@ -1,23 +1,20 @@
 import { ExecutionEnvironment } from "@/types/executor";
-import puppeteer from "puppeteer";
+import puppeteer from "puppeteer-core";
 import { LaunchBrowserTask } from "../task/LaunchBrowser";
 
-
-
-const BROWSER_WS = "wss://brd-customer-hl_5319d92f-zone-scraping_browser1:pdz89yo1j5sb@brd.superproxy.io:9222";
-  
-
+const BROWSER_WS = "wss://production-sfo.browserless.io?token=2TRz5dsbx4otgyc9656931d4e3fbe01862ce4dd273e3f8b70";
 
 export async function LaunchBrowserExecutor(environment:ExecutionEnvironment<typeof LaunchBrowserTask>):Promise<boolean>{
    try{
     const websiteUrl = environment.getInput("Website Url");
     // console.log(websiteUrl);
-    const browser = await puppeteer.launch({
+    const browser = await puppeteer.connect({
         browserWSEndpoint:BROWSER_WS,
-        headless:true,
+        // headless:true,
     });
     environment.log.info("Browser Started Successfully");
     environment.setBrowser(browser);
+
     const page = await browser.newPage();
     page.setViewport({width:1080,height:1024});
     await page.goto(websiteUrl);
